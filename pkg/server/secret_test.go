be7877978fd99e02c36e1ec2f861312abbe5cd8e
stefanprodan <stefan.prodan@gmail.com>
2019-01-06 16:41:21 +0200

    Rename secret API test to match the handler name
    Signed-off-by: stefanprodan <stefan.prodan@gmail.com>

--- pkg/server/secret_test.go
+++ pkg/server/secret_test.go
@@ -12,8 +12,8 @@ import (
 	"testing"
 )
 
-func Test_SecretsHandler(t *testing.T) {
-	namespace := "of-fnc"
+func Test_makeSecretHandler(t *testing.T) {
+	namespace := "openfaas-fn"
 	kube := testclient.NewSimpleClientset()
 	secretsHandler := makeSecretHandler(namespace, kube).ServeHTTP
 
@@ -22,7 +22,7 @@ func Test_SecretsHandler(t *testing.T) {
 	t.Run("create managed secrets", func(t *testing.T) {
 		secretValue := "testsecretvalue"
 		payload := fmt.Sprintf(`{"name": "%s", "value": "%s"}`, secretName, secretValue)
-		req := httptest.NewRequest("POST", "http://example.com/foo", strings.NewReader(payload))
+		req := httptest.NewRequest("POST", "http://system/secrets", strings.NewReader(payload))
 		w := httptest.NewRecorder()
 
 		secretsHandler(w, req)
@@ -34,7 +34,7 @@ func Test_SecretsHandler(t *testing.T) {
 
 		actualSecret, err := kube.CoreV1().Secrets(namespace).Get("testsecret", metav1.GetOptions{})
 		if err != nil {
-			t.Errorf("error validting secret: %s", err)
+			t.Errorf("error validating secret: %s", err)
 		}
 
 		if actualSecret.Name != "testsecret" {
@@ -55,7 +55,7 @@ func Test_SecretsHandler(t *testing.T) {
 	t.Run("update managed secrets", func(t *testing.T) {
 		newSecretValue := "newtestsecretvalue"
 		payload := fmt.Sprintf(`{"name": "%s", "value": "%s"}`, secretName, newSecretValue)
-		req := httptest.NewRequest("PUT", "http://example.com/foo", strings.NewReader(payload))
+		req := httptest.NewRequest("PUT", "http://system/secrets", strings.NewReader(payload))
 		w := httptest.NewRecorder()
 
 		secretsHandler(w, req)
@@ -86,7 +86,7 @@ func Test_SecretsHandler(t *testing.T) {
 	})
 
 	t.Run("list managed secrets only", func(t *testing.T) {
-		req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+		req := httptest.NewRequest("GET", "http://system/secrets", nil)
 		w := httptest.NewRecorder()
 
 		secretsHandler(w, req)
@@ -117,7 +117,7 @@ func Test_SecretsHandler(t *testing.T) {
 	t.Run("delete managed secrets", func(t *testing.T) {
 		secretName := "testsecret"
 		payload := fmt.Sprintf(`{"name": "%s"}`, secretName)
-		req := httptest.NewRequest("DELETE", "http://example.com/foo", strings.NewReader(payload))
+		req := httptest.NewRequest("DELETE", "http://system/secrets", strings.NewReader(payload))
 		w := httptest.NewRecorder()
 
 		secretsHandler(w, req)
