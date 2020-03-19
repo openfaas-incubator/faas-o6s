ce1ce0da5348b82d3517925570778e1c43e12271
stefanprodan <stefan.prodan@gmail.com>
2020-03-19 14:16:33 +0200

    Mark optional CRD fields
    Signed-off-by: stefanprodan <stefan.prodan@gmail.com>

--- pkg/apis/openfaas/v1/types.go
+++ pkg/apis/openfaas/v1/types.go
@@ -18,17 +18,26 @@ type Function struct {
 
 // FunctionSpec is the spec for a Function resource
 type FunctionSpec struct {
-	Name                   string             `json:"name"`
-	Image                  string             `json:"image"`
-	Handler                string             `json:"handler"`
-	Annotations            *map[string]string `json:"annotations"`
-	Labels                 *map[string]string `json:"labels"`
-	Environment            *map[string]string `json:"environment"`
-	Constraints            []string           `json:"constraints"`
-	Secrets                []string           `json:"secrets"`
-	Limits                 *FunctionResources `json:"limits"`
-	Requests               *FunctionResources `json:"requests"`
-	ReadOnlyRootFilesystem bool               `json:"readOnlyRootFilesystem"`
+	Name  string `json:"name"`
+	Image string `json:"image"`
+	// +optional
+	Handler string `json:"handler,omitempty"`
+	// +optional
+	Annotations *map[string]string `json:"annotations,omitempty"`
+	// +optional
+	Labels *map[string]string `json:"labels,omitempty"`
+	// +optional
+	Environment *map[string]string `json:"environment,omitempty"`
+	// +optional
+	Constraints []string `json:"constraints,omitempty"`
+	// +optional
+	Secrets []string `json:"secrets,omitempty"`
+	// +optional
+	Limits *FunctionResources `json:"limits,omitempty"`
+	// +optional
+	Requests *FunctionResources `json:"requests,omitempty"`
+	// +optional
+	ReadOnlyRootFilesystem bool `json:"readOnlyRootFilesystem"`
 }
 
 // FunctionResources is used to set CPU and memory limits and requests
